ab309e33bc945413801caffd91c4a797e6c77c5c
askupkov <alexfeed05@mail.ru>
2022-11-12 22:51:54 +0300

    №2

---
+++ PZ_5/PZ_5_1.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+print('Функция напечатает 40 рандомных символов:')
+def random_string(length): #функция которая создаёт рандомный символ
+    letter = string.ascii_letters
+    random.str = ''.join(random.choice(letter) for i in range(length))
+    print(random.str)
+
+random_string(40) #находим 40 рандомных символов
